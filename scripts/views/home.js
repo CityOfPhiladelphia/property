4d4ec614770ed1fe8cae3b9a4de5ac9f1e3f04e5
Tim Wisniewski <tim@timwis.com>
2013-11-26 14:12:01 -0500

    Removed node_modules and build

--- scripts/views/home.js
+++ scripts/views/home.js
@@ -34,39 +34,39 @@ define([
             switch(method) {
                 case "address":
                     var addressNode = e.currentTarget.address
-                        ,address = addressNode.value
+                        ,address = this.sanitize(addressNode.value)
                         ,unit = e.currentTarget.unit.value;
                     if( ! address) return $(addressNode).focus();
                     Backbone.history.navigate("/address/" + address + (unit ? "/" + unit : ""), {trigger: true});
                     break;
                 case "account":
                     var accountNode = e.currentTarget.account
-                        ,account = accountNode.value;
+                        ,account = this.sanitize(accountNode.value, "number");
                     if( ! account) return $(accountNode).focus();
                     Backbone.history.navigate("/account/" + account, {trigger: true});
                     break;
                 case "block":
                     var hundredNode = e.currentTarget.hundred
-                        ,hundred = hundredNode.value
+                        ,hundred = this.sanitize(hundredNode.value, "number")
                         ,streetNode = e.currentTarget.street
-                        ,street = streetNode.value;
+                        ,street = this.sanitize(streetNode.value);
                     if( ! hundred) return $(hundredNode).focus();
                     if( ! street) return $(streetNode).focus();
                     Backbone.history.navigate("/block/" + hundred + "/" + street, {trigger: true}); // Combine to one field as per API spec
                     break;
                 case "intersection":
                     var street1Node = e.currentTarget.street1
-                        ,street1 = street1Node.value
+                        ,street1 = this.sanitize(street1Node.value)
                         ,street2Node = e.currentTarget.street2
-                        ,street2 = street2Node.value;
+                        ,street2 = this.sanitize(street2Node.value);
                     if( ! street1) return $(street1Node).focus();
                     if( ! street2) return $(street2Node).focus();
                     Backbone.history.navigate("/intersection/" + street1 + "/" + street2, {trigger: true}); // Combine to one field as per API spec
                     break;
             }
         }
-        ,sanitize: function(input, type) { // TODO: Remove double spaces (should this allow hyphens?)
-            return typeof input === "string" ? input.replace(type === "actnum" ? /[^\d]/g : /[^\w\d\s-]/g, "") : "";
+        ,sanitize: function(input, type) {
+            return typeof input === "string" ? input.trim().replace(type === "number" ? /[^\d]/g : /[^\w\d\s-]/g, "") : "";
         }
         ,onClickLanguage: function(e) {
             e.preventDefault();
